da46c319aab1a02eace8327d2156094086f70713
Matias Báez <matiasbaez2512@gmail.com>
2019-06-22 12:08:43 -0400

    Update PublicacionesController

--- app/Http/Controllers/PlantacionesController.php
+++ app/Http/Controllers/PlantacionesController.php
@@ -3,82 +3,86 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Publicaciones;
+use Carbon\Carbon;
 
-class PlantacionesController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
+class PlantacionesController extends Controller {
+
+    public function index() {
+        $plantaciones = Publicaciones::get()->where('estado', '=', 1);
+
+        return response()->json($plantaciones, 200);
+    }
+
+    public function create() {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
+    public function store(Request $request) {
+        $idUsuario = $request->get("idUsuario");
+        $idPlanta = $request->get("idPlanta");
+        $idZona = $request->get("idZona");
+        $descripcion = $request->get("descripcion");
+
+        $plantacion = new Publicaciones();
+        $plantacion->idUsuario = $idUsuario;
+        $plantacion->idPlanta = $idPlanta;
+        $plantacion->idZona = $idZona;
+        $plantacion->descripcion = $descripcion;
+
+        if ($plantacion->save()) return response()->json(200);
+
+        return response()->json(500);
+    }
+
+    public function show($id) {
+        $plantacion = Publicaciones::findOrFail($id);
+        
+        if ($plantacion) {
+            $data = [
+                'idUsuario' => $plantacion->idUsuario,
+                'idPlanta' => $plantacion->idPlanta,
+                'idZona' => $plantacion->idZona,
+                'descripcion' => $plantacion->descripcion,
+                'estado' => $plantacion->estado
+            ];
+            return response()->json($data);
+        }
+
+        return response()->json(404);
+    }
+
+    public function edit($id) {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+    public function update(Request $request, $id) {
+        $idUsuario = $request->get("idUsuario");
+        $idPlanta = $request->get("idPlanta");
+        $idZona = $request->get("idZona");
+        $descripcion = $request->get("descripcion");
+
+        $plantacion = Publicaciones::findOrFail($id);
+
+        if ($plantacion) {
+            $plantacion->idPlanta = $idPlanta;
+            $plantacion->idZona = $idZona;
+            $plantacion->descripcion = $descripcion;
+
+            if ($plantacion->update()) return response()->json(200);
+
+            return response()->json(500);
+        }
+
+        return response()->json(500);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+    public function destroy($id) {
+        $plantacion = Publicaciones::findOrFail($id);
+        $plantacion->estado = 0;
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+        if ($plantacion->update()) return response()->json(200);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return response()->json(500);
     }
 }
